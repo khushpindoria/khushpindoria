b35d65d79281f26c68c2f6979fca6055f0024545
Khush Pindoria <khush23@hotmail.com>
2025-11-06 17:32:56 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
-  experimental: {
-    // This is to force smaller file chunks to avoid Cloudflare's 25MB limit.
-    largePageDataBytes: 128 * 1000, 
+  webpack: (config, { isServer }) => {
+    config.optimization.splitChunks = {
+      ...config.optimization.splitChunks,
+      chunks: 'all',
+    };
+    return config;
   },
 };
 
