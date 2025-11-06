7c03726adf71faba5c22019b9eaa9a560b3cf8ef
Khush Pindoria <khush23@hotmail.com>
2025-11-06 21:11:56 +0800

    image replace

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,14 +4,16 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2">
-      <Image
-        src="/img/kp-intials.png"
-        alt="Logo"
-        width={32}
-        height={32}
-        priority
-        className="h-8 w-8"
-      />
+      <div className="h-8 w-8 bg-red-500">
+        <Image
+          src="/img/kp-initials.png"
+          alt="Logo"
+          width={32}
+          height={32}
+          priority
+          className="h-8 w-8"
+        />
+      </div>
       <span className="text-xl font-bold tracking-tighter text-primary">
         KP
       </span>
