7d865eb11c927be85f064443f62b57ccca92068d
Khush Pindoria <khush23@hotmail.com>
2025-11-06 18:16:55 +0800

    26mb error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
@@ -10,38 +12,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
-      {
+       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-      }
+      },
     ],
   },
-  webpack: (config, {isServer}) => {
-    config.optimization.splitChunks = {
-      ...config.optimization.splitChunks,
-      cacheGroups: {
-        ...config.optimization.splitChunks.cacheGroups,
-        // Force xterm into a separate chunk
-        xterm: {
-          test: /[\\/]node_modules[\\/](@xterm|xterm-addon-fit)[\\/]/,
-          name: 'xterm',
-          chunks: 'all',
-          priority: 20,
-        },
-        // Force all node_modules into a vendor chunk
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)?.[1];
-            return `npm.${packageName?.replace('@', '')}`;
-          },
-          chunks: 'all',
-          priority: 10,
-        },
-      },
-    };
-    return config;
-  }
 };
 
 export default nextConfig;
