211eef41ebd59d6b679a5c1eb59484bf5333f6cc
Khush Pindoria <khush23@hotmail.com>
2025-11-06 21:30:12 +0800

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,18 +1,9 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Logo() {
   return (
-    <Link href="/" className="flex items-center space-x-2">
-      <Image
-        src="/img/kp-initials.png"
-        alt="Logo"
-        width={32}
-        height={32}
-        priority
-        className="h-8 w-8"
-      />
-      <span className="text-xl font-normal tracking-wider text-primary font-logo">
+    <Link href="/" className="flex items-center">
+      <span className="text-3xl font-normal tracking-wider text-primary font-logo">
         KP
       </span>
     </Link>
